569226b7d21e407cd5a0743f251205424d659497
afifahfikri <49128141+afifahfikri@users.noreply.github.com>
2019-04-07 21:14:42 +0700

    Update routingManual.c

--- routingManual.c
+++ routingManual.c
@@ -1,5 +1,6 @@
 //4. Routing Manual
 #include <stdio.h>
+#include <stdlib.h>
 #include "variable.h"
 
 typedef struct 
@@ -11,7 +12,7 @@ void routingManual (int n, int m, jalur pcb[40][40])
 {
 	koordinat koordinatKe[100];
 	int jumlahKoor,i,kolom,baris;
-	int a,b; //menyimpan koordinat sementara
+	char a,b; //menyimpan koordinat sementara
 	char sym;//menyimpan simbol sementara
 	printf ("Isi `q` atau `Q` untuk kembali ke menu\n");
 	printf ("Isi `n` atau `N` untuk memulai pada simpul (node) baru\n");
@@ -30,15 +31,15 @@ void routingManual (int n, int m, jalur pcb[40][40])
 			printf ("Koordinat %d : ",jumlahKoor);
 			do
 			{
-				scanf (" %d%*c%d", &a,&b);
+				scanf (" %c%*c%c", &a,&b);
 			}
-			while ((a>n && a<0 && b>m && b<0) || (pcb[a][b].listrik.nama==' '));//validasi matriks
-			koordinatKe[jumlahKoor].x=a;
-			koordinatKe[jumlahKoor].y=b;
+			while ((atoi(a)>n && atoi(a)<0 && atoi(b)>m && atoi(b)<0) || (pcb[atoi(a)][atoi(b)].listrik.nama==' '));//validasi matriks
+			koordinatKe[jumlahKoor].x=atoi(a);
+			koordinatKe[jumlahKoor].y=atoi(b);
 			jumlahKoor++;
 		}
 		while (!(a=='n'||a=='N'||b=='n'||b=='N'));//validasi node
-		if (!(a=='n'||a=='N'||b=='n'||b=='N'))
+		if ((a=='n'||a=='N'||b=='n'||b=='N'))
 		{
 			for (i=1;i<jumlahKoor;i++)
 			{
