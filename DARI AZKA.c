709621ae1b7ec5501db09a277be87661ebcc6c97
azkarain <44610850+azkarain@users.noreply.github.com>
2019-04-07 19:16:48 +0700

    Update and rename coba_2.c to DARI AZKA.c

--- DARI AZKA.c
+++ DARI AZKA.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include "lib.h"
 #include <math.h>
 
-void make_layout(int n, int m,jalur pcb[40][40]) {
+void make_layout(int n, int m,jalur pcb[40][40], int *x) {
 	komponen masuk;
-	int inp_x1, inp_y1, inp_x2, inp_y2,i,j;
+	int inp_x1, inp_y1, inp_x2, inp_y2;
 	double jarak;
 	
-	for (j=0; j<m; j+=1) {
-		for(i=0; i<n ; i+=1){
-			pcb[i][j].listrik.nomor = NULL;
-		}
-	}
-	
+	*x=0;
 	
 	printf("[Mode Layout]\n");
 	printf("Isi 'q' atau 'Q' untuk kembali ke menu\n"); /*WARNING!!! MASUKKAN JUGA ANGKA SETELAH Q ATAU q */
@@ -36,6 +30,7 @@ void make_layout(int n, int m,jalur pcb[40][40]) {
 					pcb[inp_x1-1][inp_y1-1].listrik.nomor=masuk.nomor;
 					pcb[inp_x2-1][inp_y2-1].listrik.nama=masuk.nama;
 					pcb[inp_x2-1][inp_y2-1].listrik.nomor=masuk.nomor;
+					*x+=1;
 					printf("\nPilih Komponen (R,C,T,J): "); scanf(" %c%c", &masuk.nama, &masuk.nomor);
 				}
 				else if (masuk.nama == 'C' && (jarak>=2)) {
@@ -44,6 +39,7 @@ void make_layout(int n, int m,jalur pcb[40][40]) {
 					pcb[inp_x1-1][inp_y1-1].listrik.nomor=masuk.nomor;
 					pcb[inp_x2-1][inp_y2-1].listrik.nama=masuk.nama;
 					pcb[inp_x2-1][inp_y2-1].listrik.nomor=masuk.nomor;
+					*x+=1;
 					printf("\nPilih Komponen (R,C,T,J): "); scanf(" %c%c", &masuk.nama, &masuk.nomor);
 				}
 				else if (masuk.nama == 'T' && (jarak>=2)) {
@@ -52,6 +48,7 @@ void make_layout(int n, int m,jalur pcb[40][40]) {
 					pcb[inp_x1-1][inp_y1-1].listrik.nomor=masuk.nomor;
 					pcb[inp_x2-1][inp_y2-1].listrik.nama=masuk.nama;
 					pcb[inp_x2-1][inp_y2-1].listrik.nomor=masuk.nomor;
+					*x+=1;
 					printf("\nPilih Komponen (R,C,T,J): "); scanf(" %c%c", &masuk.nama, &masuk.nomor);
 				}
 				else if (masuk.nama == 'J' && (jarak>=1)) {
@@ -60,6 +57,7 @@ void make_layout(int n, int m,jalur pcb[40][40]) {
 					pcb[inp_x1-1][inp_y1-1].listrik.nomor=masuk.nomor;
 					pcb[inp_x2-1][inp_y2-1].listrik.nama=masuk.nama;
 					pcb[inp_x2-1][inp_y2-1].listrik.nomor=masuk.nomor;
+					*x+=1;
 					printf("\nPilih Komponen (R,C,T,J): "); scanf(" %c%c", &masuk.nama, &masuk.nomor);
 				}
 				else {
@@ -98,11 +96,93 @@ void print_layout(int n, int m, jalur pcb[40][40]){
 	
 }
 
-int main () {
+void auto_layout(jalur pcb [40][40]) {
+	pcb[4][0].listrik.nama = 'R';
+	pcb[4][0].listrik.nomor = '1';
+	pcb[4][4].listrik.nama = 'R';
+	pcb[4][4].listrik.nomor = '1';
 	
-	make_layout(5,10, pcb);
+	pcb[5][0].listrik.nama = 'J';
+	pcb[5][0].listrik.nomor = '1';
+	pcb[5][9].listrik.nama = 'J';
+	pcb[5][9].listrik.nomor = '1';
 	
-	print_layout(5,10,pcb);
+	pcb[6][5].listrik.nama = 'C';
+	pcb[6][5].listrik.nomor = '1';
+	pcb[8][5].listrik.nama = 'C';
+	pcb[8][5].listrik.nomor = '1';
 	
-	return 0;
+	pcb[6][6].listrik.nama = 'T';
+	pcb[6][6].listrik.nomor = '1';
+	pcb[6][8].listrik.nama = 'T';
+	pcb[6][8].listrik.nomor = '1';
+	
+	pcb[6][0].listrik.nama = 'R';
+	pcb[6][0].listrik.nomor = '2';
+	pcb[6][4].listrik.nama = 'R';
+	pcb[6][4].listrik.nomor = '2';
+	
+	pcb[4][5].listrik.nama = 'R';
+	pcb[4][5].listrik.nomor = '3';
+	pcb[4][9].listrik.nama = 'R';
+	pcb[4][9].listrik.nomor = '3';
+	
+	pcb[7][8].listrik.nama = 'R';
+	pcb[7][8].listrik.nomor = '4';
+	pcb[7][11].listrik.nama = 'R';
+	pcb[7][11].listrik.nomor = '4';
+	
+	pcb[9][5].listrik.nama = 'J';
+	pcb[9][5].listrik.nomor = '2';
+	pcb[5][10].listrik.nama = 'J';
+	pcb[5][10].listrik.nomor = '2';
+	
+	pcb[0][4].listrik.nama = 'J';
+	pcb[0][4].listrik.nomor = '3';
+	pcb[5][11].listrik.nama = 'J';
+	pcb[5][11].listrik.nomor = '3';
+	
+	pcb[1][4].listrik.nama = 'C';
+	pcb[1][4].listrik.nomor = '2';
+	pcb[3][4].listrik.nama = 'C';
+	pcb[3][4].listrik.nomor = '2';
+	
+	pcb[8][8].listrik.nama = 'C';
+	pcb[8][8].listrik.nomor = '3';
+	pcb[8][10].listrik.nama = 'C';
+	pcb[8][10].listrik.nomor = '3';
+	
+}
+
+void auto_routing(jalur pcb[40][40]) {
+	pcb[4][0].simbol = '*';
+	pcb[5][0].simbol = '*';
+	pcb[6][0].simbol = '*';
+	
+	pcb[0][4].simbol = '!';
+	pcb[1][4].simbol = '!';
+	
+	pcb[3][4].simbol = '@';
+	pcb[4][4].simbol = '@';
+	pcb[4][5].simbol = '@';
+	
+	pcb[6][4].simbol = '#';
+	pcb[6][5].simbol = '#';
+	pcb[6][6].simbol = '#';
+	
+	pcb[8][5].simbol = '$';
+	pcb[9][5].simbol = '$';
+	
+	pcb[6][8].simbol = '%';
+	pcb[7][8].simbol = '%';
+	pcb[8][8].simbol = '%';
+	
+	pcb[4][9].simbol = '^';
+	pcb[5][9].simbol = '^';
+	pcb[5][10].simbol = '^';
+	pcb[5][11].simbol = '^';
+	pcb[6][11].simbol = '^';
+	pcb[7][11].simbol = '^';
+	pcb[8][11].simbol = '^';
+	pcb[8][10].simbol = '^';
 }
