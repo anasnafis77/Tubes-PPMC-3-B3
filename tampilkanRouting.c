050c85e956c30d412237716a60c58e2ffdfc56ba
afifahfikri <49128141+afifahfikri@users.noreply.github.com>
2019-04-07 17:40:50 +0700

    Update tampilkanRouting.c

--- tampilkanRouting.c
+++ tampilkanRouting.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include "variable.h"
 
-void tampilkanRouting ()
-{
+void tampilkanRouting(int n, int m, jalur pcb[40][40]){
 	int i,j;
-	for (i=0;i<40;i++)
-	{
-		for (j=0;j<40;j++)
-		{
-			printf ("%c  ",pcb[i][j].simbol);
-		}
-		printf (" \n");
+	
+	for (i=0; i<n; i+=1){
+		printf("	%d", i+1);
 	}
+	printf("\n");
+	
+	for (j=0; j<m; j+=1) {
+		printf("%d	", j+1);
+		for(i=0; i<n ; i+=1){
+			printf("%c	", pcb[i][j].listrik.simbol);
+		}
+		printf("\n");
+	}
+	
 }
