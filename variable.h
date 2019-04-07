ba9e354f04410c2f231166a940501ef45a511205
resfy <44311866+resfy@users.noreply.github.com>
2019-04-07 17:45:54 +0700

    Update variable.h

--- variable.h
+++ variable.h
@@ -1,9 +1,14 @@
+#ifndef LIB_H
+#define LIB_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 
 typedef struct {//nama komponen yang terdiri atas huruf simbol komponen dan nomor komponen
 	char nama;
-	int nomor;
+	char nomor;
 }komponen;
 
 typedef struct {//tipe data untuk menyatakan komponen dan simbol jalur pada PCB
@@ -11,6 +16,13 @@ typedef struct {//tipe data untuk menyatakan komponen dan simbol jalur pada PCB
 	char simbol;
 }jalur;
 
-jalur pcb [40][40]; //matriks PCB 
+jalur pcb[40][40]; //matriks PCB 
 
+char fileName[20];
+int n, m;
 
+//deklarasi fungsi
+void routingManual (jalur pcb[40][40]);
+void make_layout(int n, int m,jalur pcb[40][40]);
+
+#endif
