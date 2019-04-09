5ad3ec9515269dac02228e2260a22a5cb877e0af
haninanz <44562014+haninanz@users.noreply.github.com>
2019-04-10 00:55:50 +0700

    Add files via upload

---
+++ saveProjectnew.c
@@ -0,0 +1,34 @@
+//Library yg dipake
+#include <stdio.h>
+#include <string.h>
+
+//Implementasi saveProject
+void saveProject(jalur pcb[][40], char filename[], int neff){
+	FILE *flayout, *froute;
+	char temp1, temp2, temp[3];
+	char file[20];
+	
+	strcpy(file, filename);
+	flayout = fopen(strcat(file, "_layout.csv"), "w");
+	//Memindahkan isi array ke excel
+	for(int i = 0; i < neff; i++){
+		for(int j = 0; j < neff; j++){
+			temp1 = pcb[i][j].listrik.nama;
+			temp2 = pcb[i][j].listrik.nomor;
+			temp[0] = temp1;
+			temp[1] = temp2;
+			temp[2] = '\0';
+			fprintf(flayout, "%s", temp);
+		}
+	}
+	fclose(flayout);
+	froute = fopen(strcat(filename, "_routing.csv"), "w");
+	for(int i = 0; i < neff; i++){
+		for(int j = 0; j < neff; j++){
+			fprintf(flayout, "%c", pcb[i][j].simbol);
+		}
+	}
+	fclose(froute);
+	
+	return;
+}
