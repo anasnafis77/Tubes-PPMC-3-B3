aaa1148bbb3104a2917067440de3366e611a9f84
haninanz <44562014+haninanz@users.noreply.github.com>
2019-04-10 01:07:53 +0700

    Update saveProjectnew.c

--- saveProjectnew.c
+++ saveProjectnew.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 //Implementasi saveProject
-void saveProject(jalur pcb[][40], char filename[], int neff){
+void saveProject(jalur pcb[][40], char filename[], int n, int m){
 	FILE *flayout, *froute;
 	char temp1, temp2, temp[3];
 	char file[20];
@@ -11,8 +11,8 @@ void saveProject(jalur pcb[][40], char filename[], int neff){
 	strcpy(file, filename);
 	flayout = fopen(strcat(file, "_layout.csv"), "w");
 	//Memindahkan isi array ke excel
-	for(int i = 0; i < neff; i++){
-		for(int j = 0; j < neff; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
 			temp1 = pcb[i][j].listrik.nama;
 			temp2 = pcb[i][j].listrik.nomor;
 			temp[0] = temp1;
@@ -23,8 +23,8 @@ void saveProject(jalur pcb[][40], char filename[], int neff){
 	}
 	fclose(flayout);
 	froute = fopen(strcat(filename, "_routing.csv"), "w");
-	for(int i = 0; i < neff; i++){
-		for(int j = 0; j < neff; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
 			fprintf(flayout, "%c", pcb[i][j].simbol);
 		}
 	}
