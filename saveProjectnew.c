9309ce216853f01c67223006e0f706e6be24ae32
haninanz <44562014+haninanz@users.noreply.github.com>
2019-04-10 08:46:58 +0700

    Update saveProjectnew.c

--- saveProjectnew.c
+++ saveProjectnew.c
@@ -1,6 +1,7 @@
 //Library yg dipake
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 //Implementasi saveProject
 void saveProject(jalur pcb[][40], char filename[], int n, int m){
@@ -13,19 +14,31 @@ void saveProject(jalur pcb[][40], char filename[], int n, int m){
 	//Memindahkan isi array ke excel
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
-			temp1 = pcb[i][j].listrik.nama;
-			temp2 = pcb[i][j].listrik.nomor;
-			temp[0] = temp1;
-			temp[1] = temp2;
-			temp[2] = '\0';
-			fprintf(flayout, "%s", temp);
+			fprintf(flayout, ",");
+			if(pcb[i][j].listrik.nama == ' '){
+				fprintf(flayout, ",");
+			}
+			else{
+				temp1 = pcb[i][j].listrik.nama;
+				temp2 = pcb[i][j].listrik.nomor;
+				temp[0] = temp1;
+				temp[1] = temp2;
+				temp[2] = '\0';
+				fprintf(flayout, "%s", temp);
+			}
 		}
 	}
 	fclose(flayout);
 	froute = fopen(strcat(filename, "_routing.csv"), "w");
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
-			fprintf(flayout, "%c", pcb[i][j].simbol);
+			fprintf(flayout, ",");
+			if(pcb[i][j].simbol == ' '){
+				fprintf(froute, ",");
+			}
+			else{
+				fprintf(froute, "%c", pcb[i][j].simbol);
+			}
 		}
 	}
 	fclose(froute);
