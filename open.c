f1b26e13da402b9af8c7c7e94ab1d2e78e9a2170
haninanz <44562014+haninanz@users.noreply.github.com>
2019-04-10 09:41:01 +0700

    Update open.c

--- open.c
+++ open.c
@@ -1,6 +1,8 @@
-void open(){
+void open(int n, int m){
 	FILE * fptr;
 	char filename[20];
+	int i, j;
+	char c;
 	
 	printf("Masukkan nama file yang ingin dibuka: ");
 	do{
@@ -11,6 +13,24 @@ void open(){
 			printf("File tidak tersedia.\n");
 		}
 	}while(fptr == NULL);
+	
+	//Memasukkan dari file eksternal ke program
+	for(i = 0; i < n; i++){
+		if(i == 0){
+			for(j = 0; j < m; j++){
+				printf("%d\t", j);
+			}	
+		}
+		printf("\n");
+		for(j = 0; j < m; j++){
+			printf("%d\t", i);
+			c = fgetc(fptr);
+			if(feof(fptr)){
+				break;
+			}
+			printf("%c\t", c);
+		}
+	}		
 	
 	return;
 }
